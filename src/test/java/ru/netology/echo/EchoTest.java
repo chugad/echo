f1538c0a79d328d5d47a5b313127938df8b0bf54
Oleg Dudin <o.v.dudin@yandex.ru>
2020-11-01 21:24:58 +0700

    Передал на добавление в поле body неверное выражение.

--- src/test/java/ru/netology/echo/EchoTest.java
+++ src/test/java/ru/netology/echo/EchoTest.java
@@ -14,17 +14,14 @@ public class EchoTest {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("foo1=bar1, foo2=bar2") // отправляемые данные (заголовки и query можно выставлять аналогично)
-                .header("Content-Type", "application/json; charset=utf-8")
-                .header("Connection", "keep-alive")
+                .body("ffааааааййййй") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("foo1=bar1, foo2=bar2"))
-                .header("Content-Type", "application/json; charset=utf-8")
+                .body("data", equalTo("ffааааааййййй"))
         ;
     }
 }
