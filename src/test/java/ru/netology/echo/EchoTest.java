295a8a4c40c7c10842a1a2f1a35d7bed2e681dd6
Oleg Dudin <o.v.dudin@yandex.ru>
2020-11-01 21:29:21 +0700

    Передал на добавление в поле body верное выражение.

--- src/test/java/ru/netology/echo/EchoTest.java
+++ src/test/java/ru/netology/echo/EchoTest.java
@@ -14,14 +14,14 @@ public class EchoTest {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("ffааааааййййй") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("ff") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("ffааааааййййй"))
+                .body("data", equalTo("ff"))
         ;
     }
 }
